9af149f8c46a96d6ab462c27ba4d7453066ac96e
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-12-28 11:37:03 -0600

    added formatCurrency function to utilities folder. takes in price and uses Intl.NumberFormat to return formatted price in USD

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
     id: number,
@@ -9,6 +10,19 @@ type StoreItemProps = {
 
 export function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
     return <Card>
-        <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover"}} />
+        <Card.Img 
+            variant="top" 
+            src={imgUrl} 
+            height="200px" 
+            style={{ objectFit: "cover"}} 
+        />
+        <Card.Body className="d-flex flex-column">
+            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                {/* item title */}
+                <span className="fs-2">{name}</span>
+                {/* ms is margin start, so title doesn't touch price */}
+                <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+            </Card.Title>
+        </Card.Body>
     </Card>
 }
