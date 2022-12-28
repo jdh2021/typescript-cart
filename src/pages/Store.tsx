f4f90e10f6139ed53e3d8627c7e46db2ad7d5f69
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-12-28 11:25:17 -0600

    added data for items in items.json file. mapped over storeItems array. created storeItem component and confirmed images displaying correctly

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,19 @@
+import storeItems from "../data/items.json"
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
 
 export function Store() {
-    return <h1>Store</h1>
+    return (
+        <><h1>Store</h1>
+            <Row lg={3} md={2} xs={1} className="g-2">
+                {/* map over array of items */}
+                {storeItems.map(item => (
+                    <Col key={item.id}>
+                        {/* StoreItem takes in as a prop all of the item properties */}
+                        <StoreItem {...item} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
 }
